ee95d44cdb2e9c8579d93e68a339353b066112ad
Tucker <trblackw@ncsu.edu>
2019-02-05 08:35:31 -0500

    established new instance of apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./spacex.png";
+import styled from "styled-components";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Launches from "./components/Launches";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-         hey
-      </div>
-    );
-  }
-}
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+const App = () => (
+  <ApolloProvider client={client}>
+    <AppContainer>
+      <img src={logo} alt="spacex-logo" />
+      <Launches />
+    </AppContainer>
+  </ApolloProvider>
+);
 
 export default App;
+
+const AppContainer = styled("div")`
+  margin: 0 auto;
+  padding: 1em;
+
+  img {
+    width: 300px;
+    display: block;
+    margin: auto;
+  }
+`;
