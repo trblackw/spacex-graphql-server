7bfa09a5d3a3602de23f61e5e85de7954a9b899f
Tucker <trblackw@ncsu.edu>
2019-02-08 08:12:41 -0500

    using cors to nullify server issue

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require("express")(),
   graphqlHTTP = require("express-graphql"),
+  cors = require("cors"),
   PORT = process.env.PORT || 5000,
   schema = require("./schema");
-
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
