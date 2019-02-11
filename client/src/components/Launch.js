148a2f4673a6eee8bad03c9a341b920e8cdb7a8c
Tucker <trblackw@ncsu.edu>
2019-02-11 18:37:34 -0500

    modified launch container

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,45 +1,43 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const Launch = ({
-  flight_number,
-  mission_name,
-  launch_date_local,
-  launch_success
-}) => (
-  <LaunchContainer>
-    <h1>{mission_name}</h1>
-    <div>
-      <span>{flight_number}</span>
-      <span>{launch_date_local}</span>
-      <span>{launch_success}</span>
-    </div>
-  </LaunchContainer>
+const Launch = ({flight_number, mission_name, launch_date_local, launch_success}) => (
+	<LaunchContainer>
+		<h1>{mission_name}</h1>
+		<div>
+			<span>{flight_number}</span>
+			<span>{launch_date_local}</span>
+			<span>{launch_success}</span>
+		</div>
+	</LaunchContainer>
 );
 
 export default Launch;
 
-const LaunchContainer = styled("div")`
-  width: 70%;
-  border-radius: 5px;
-  margin: 1em auto;
-  padding: 1.5em;
-  background: hsl(0, 0%, 51%);
-  color: whitesmoke;
-  &:hover {
-    transform: translateY(-5px);
-    transition-duration: 300ms;
-    cursor: pointer;
-  }
+const LaunchContainer = styled('div')`
+	width: 70%;
+	border-radius: 5px;
+	margin: 1em auto;
+	padding: 1.5em;
+	background: hsl(0, 0%, 51%);
+	color: whitesmoke;
+	&:hover {
+		transform: translateY(-5px);
+		transition-duration: 300ms;
+		cursor: pointer;
+	}
 
-  div {
-    display: flex;
-    justify-content: flex-end;
-    align-content: center;
-  }
+	div {
+		width: 50%;
+		display: flex;
+      margin: 0 auto;
+		justify-content: flex-end;
+		align-content: center;
+      border: 1px solid red;
+	}
 
-  span {
-    padding: 1em;
-    margin: 1em;
-  }
+	span {
+		padding: 1em;
+		margin: 1em;
+	}
 `;
